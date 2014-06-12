f921617b48c884ebabf248b7135bf322b4eda1d2
Carlos Sandoval <sbmako@gmail.com>
2014-06-11 22:10:39 -0500

    added sample failing test to checkout appveyor

---
+++ src/NServiceBus.MongoDB.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NServiceBus.MongoDB.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("added sample fail to test appveyor");
+        }
+    }
+}
