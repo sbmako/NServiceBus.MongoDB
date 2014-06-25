30bd6a7d48dbadbb66be4a254e7fb95cb0664e62
Carlos Sandoval <sbmako@gmail.com>
2014-06-25 08:08:39 -0500

    added autofixture

---
+++ src/NServiceBus.MongoDB.Tests/SagaPersister/MongoClientAccessorTests.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MongoClientAccessorTests.cs" company="SharkByte Software Inc.">
+//   Copyright (c) 2014 Carlos Sandoval. All rights reserved.
+//   
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Affero General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//   
+//   You should have received a copy of the GNU Affero General Public License
+//   along with this program.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+// <summary>
+//   Defines the MongoClientAccessorTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NServiceBus.MongoDB.Tests.SagaPersister
+{
+    using Ploeh.AutoFixture.Xunit;
+
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class MongoClientAccessorTests
+    {
+        [Theory, AutoData]
+        public void MongoClientAccessorConstror(string databaseName)
+        {
+        }
+    }
+}
