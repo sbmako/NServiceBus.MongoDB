9e7d1579a396ed288e6d71329dbc5ae3e49a8790
Carlos Sandoval <sbmako@gmail.com>
2015-01-30 23:00:23 -0600

    reformatted line

--- src/NServiceBus.MongoDB.Acceptance.Tests/App_Packages/NSB.AcceptanceTests.5.1.3/Sagas/When_receiving_that_should_start_a_saga.cs
+++ src/NServiceBus.MongoDB.Acceptance.Tests/App_Packages/NSB.AcceptanceTests.5.1.3/Sagas/When_receiving_that_should_start_a_saga.cs
@@ -16,15 +16,16 @@
         public void Should_start_the_saga_and_call_messagehandlers()
         {
             Scenario.Define<SagaEndpointContext>()
-                    .WithEndpoint<SagaEndpoint>(b => b.Given(bus => bus.SendLocal(new StartSagaMessage() { SomeId = Guid.NewGuid().ToString() })))
-                    .Done(context => context.InterceptingHandlerCalled && context.SagaStarted)
-                    .Repeat(r => r.For(Transports.Default))
-                    .Should(c =>
-                    {
-                        Assert.True(c.InterceptingHandlerCalled, "The message handler should be called");
-                        Assert.True(c.SagaStarted, "The saga should have been started");
-                    })
-                    .Run();
+                .WithEndpoint<SagaEndpoint>(
+                    b => b.Given(bus => bus.SendLocal(new StartSagaMessage() { SomeId = Guid.NewGuid().ToString() })))
+                .Done(context => context.InterceptingHandlerCalled && context.SagaStarted)
+                .Repeat(r => r.For(Transports.Default))
+                .Should(
+                    c =>
+                        {
+                            Assert.True(c.InterceptingHandlerCalled, "The message handler should be called");
+                            Assert.True(c.SagaStarted, "The saga should have been started");
+                        }).Run();
         }
 
 
