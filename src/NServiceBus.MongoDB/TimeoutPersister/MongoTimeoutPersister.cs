5a1198ef8e36fc0c1432046d618101472478f1f5
Carlos Sandoval <sbmako@gmail.com>
2016-10-04 23:04:25 -0500

    continued work fixing unit tests

--- src/NServiceBus.MongoDB/TimeoutPersister/MongoTimeoutPersister.cs
+++ src/NServiceBus.MongoDB/TimeoutPersister/MongoTimeoutPersister.cs
@@ -30,6 +30,7 @@ namespace NServiceBus.MongoDB.TimeoutPersister
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Diagnostics.Contracts;
     using System.Linq;
     using System.Threading.Tasks;
@@ -77,25 +78,13 @@ namespace NServiceBus.MongoDB.TimeoutPersister
         /// <returns>
         /// Returns the next range of timeouts that are due.
         /// </returns>
-        public Task<TimeoutsChunk> GetNextChunk(DateTime startSlice)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Retrieves the next range of timeouts that are due.
-        /// </summary>
-        /// <param name="startSlice"> The time where to start retrieving the next slice, the slice should exclude this date.</param>
-        /// <param name="nextTimeTorunQuery">Returns the next time we should query again.</param>
-        /// <returns>
-        /// Returns the next range of timeouts that are due.
-        /// </returns>
-        public IEnumerable<Tuple<string, DateTime>> GetNextChunk(DateTime startSlice, out DateTime nextTimeTorunQuery)
+        public async Task<TimeoutsChunk> GetNextChunk(DateTime startSlice)
         {
             var collection = this.mongoDatabase.GetCollection<TimeoutEntity>(TimeoutDataName);
 
             var now = DateTime.UtcNow;
 
+<<<<<<< 8b9e6fd26042e2be59553b8bdc139ea40e7859bd
             var results = from data in collection.AsQueryable().AssumedNotNull()
                           where data.Time > startSlice && data.Time <= now
                           where
@@ -103,18 +92,39 @@ namespace NServiceBus.MongoDB.TimeoutPersister
                               || data.OwningTimeoutManager == this.endpointName
                           orderby data.Time ascending 
                           select new Tuple<string, DateTime>(data.Id, data.Time);
+=======
+            var builder = Builders<TimeoutEntity>.Filter;
+            var query = builder.Eq(t => t.OwningTimeoutManager, this.endpointName) & builder.Gt(t => t.Time, startSlice)
+                        & builder.Lte(t => t.Time, now);
+
+            var results =
+                await
+                collection.Find(query)
+                    .Sort(Builders<TimeoutEntity>.Sort.Ascending(t => t.Time))
+                    .ToListAsync()
+                    .ConfigureAwait(false);
+
+            ////var results = from data in collection.AsQueryable().AssumedNotNull()
+            ////              where data.Time > startSlice && data.Time <= now
+            ////              where
+            ////                  data.OwningTimeoutManager == string.Empty
+            ////                  || data.OwningTimeoutManager == this.endpointName
+            ////              orderby data.Time ascending
+            ////              select new TimeoutsChunk.Timeout(data.Id, data.Time);
+>>>>>>> continued work fixing unit tests
 
             var nextTimeout = from data in collection.AsQueryable().AssumedNotNull()
                               where data.Time > now
-                              orderby data.Time ascending 
+                              orderby data.Time ascending
                               select data;
 
-            nextTimeTorunQuery = nextTimeout.Any()
-                                     ? nextTimeout.First().Time
-                                     : now.AddMinutes(
-                                         MongoPersistenceConstants.DefaultNextTimeoutIncrementMinutes);
+            var nextTimeTorunQuery = nextTimeout.Any()
+                                         ? nextTimeout.First().Time
+                                         : now.AddMinutes(MongoPersistenceConstants.DefaultNextTimeoutIncrementMinutes);
 
-            return results.ToList();
+            return new TimeoutsChunk(
+                results.Select(data => new TimeoutsChunk.Timeout(data.Id, data.Time)),
+                nextTimeTorunQuery);
         }
 
         /// <summary>
@@ -125,13 +135,13 @@ namespace NServiceBus.MongoDB.TimeoutPersister
         /// <returns>
         /// <see cref="T:NServiceBus.Timeout.Core.TimeoutEntity"/> of the timeout if it was found. <c>null</c> otherwise.
         /// </returns>
-        public Task<Timeout.Core.TimeoutData> Peek(string timeoutId, ContextBag context)
+        public Task<TimeoutData> Peek(string timeoutId, ContextBag context)
         {
             var collection = this.mongoDatabase.GetCollection<TimeoutEntity>(TimeoutDataName);
             var data = collection.AsQueryable().SingleOrDefault(e => e.Id == timeoutId);
             if (data != null)
             {
-                return Task.FromResult<Timeout.Core.TimeoutData>(new Timeout.Core.TimeoutData
+                return Task.FromResult<TimeoutData>(new TimeoutData
                 {
                     Id = data.Id,
                     Destination = data.Destination,
@@ -143,7 +153,7 @@ namespace NServiceBus.MongoDB.TimeoutPersister
                 });
             }
 
-            return Task.FromResult<Timeout.Core.TimeoutData>(null);
+            return Task.FromResult<TimeoutData>(null);
         }
         
         /// <summary>
@@ -152,7 +162,7 @@ namespace NServiceBus.MongoDB.TimeoutPersister
         /// <param name="timeout">The timeout data to add</param>
         /// <param name="context">The context</param>
         /// <returns>The task</returns>
-        public async Task Add(Timeout.Core.TimeoutData timeout, ContextBag context)
+        public async Task Add(TimeoutData timeout, ContextBag context)
         {
             var data = new TimeoutEntity()
             {
