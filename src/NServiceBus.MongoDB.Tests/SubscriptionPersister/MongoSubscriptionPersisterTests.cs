0e47b6334785c4b33fe5a5882e074be3c07bc142
Carlos Sandoval <sbmako@gmail.com>
2016-10-11 22:13:52 -0500

    updated copyright year in file headers

--- src/NServiceBus.MongoDB.Tests/SubscriptionPersister/MongoSubscriptionPersisterTests.cs
+++ src/NServiceBus.MongoDB.Tests/SubscriptionPersister/MongoSubscriptionPersisterTests.cs
@@ -2,7 +2,7 @@
 // <copyright file="MongoSubscriptionPersisterTests.cs" company="SharkByte Software">
 //   The MIT License (MIT)
 //   
-//   Copyright (c) 2015 SharkByte Software
+//   Copyright (c) 2016 SharkByte Software
 //   
 //   Permission is hereby granted, free of charge, to any person obtaining a copy of
 //   this software and associated documentation files (the "Software"), to deal in
