41787d58f864064b6c730b912ba52797a8f5350b
Carlos Sandoval <sbmako@gmail.com>
2016-10-11 22:14:05 -0500

    updated copyright year in file headers

--- src/NServiceBus.MongoDB/DataBus/MongoGridFSDataBus.cs
+++ src/NServiceBus.MongoDB/DataBus/MongoGridFSDataBus.cs
@@ -22,7 +22,7 @@
 //   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // </copyright>
 // <summary>
-//   
+//   The MongoDB GridFS data bus.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
