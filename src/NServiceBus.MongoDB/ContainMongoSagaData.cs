24de47d42c74d783ffae430e92bcae54238400b1
Carlos Sandoval <sbmako@gmail.com>
2017-01-18 12:40:57 -0600

    updated copyright notices

--- src/NServiceBus.MongoDB/ContainMongoSagaData.cs
+++ src/NServiceBus.MongoDB/ContainMongoSagaData.cs
@@ -2,7 +2,7 @@
 // <copyright file="ContainMongoSagaData.cs" company="SharkByte Software">
 //   The MIT License (MIT)
 //   
-//   Copyright (c) 2016 SharkByte Software
+//   Copyright (c) 2017 SharkByte Software
 //   
 //   Permission is hereby granted, free of charge, to any person obtaining a copy of
 //   this software and associated documentation files (the "Software"), to deal in
@@ -21,6 +21,9 @@
 //   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // </copyright>
+// <summary>
+//   Base Helper class for MongoDB saga data
+// </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace NServiceBus.MongoDB
