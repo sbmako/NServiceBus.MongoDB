5025583c7ed2ecf5983c13e4a5ef5f412f95485e
Carlos Sandoval <sbmako@gmail.com>
2018-06-30 22:02:51 -0700

    updating version and copyright info

--- src/NServiceBus.MongoDB/TimeoutPersister/MongoTimeoutPersister.cs
+++ src/NServiceBus.MongoDB/TimeoutPersister/MongoTimeoutPersister.cs
@@ -2,7 +2,7 @@
 // <copyright file="MongoTimeoutPersister.cs" company="SharkByte Software">
 //   The MIT License (MIT)
 //   
-//   Copyright (c) 2017 SharkByte Software
+//   Copyright (c) 2018 SharkByte Software
 //   
 //   Permission is hereby granted, free of charge, to any person obtaining a copy of
 //   this software and associated documentation files (the "Software"), to deal in
